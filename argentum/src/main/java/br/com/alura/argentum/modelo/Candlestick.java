7cc276b3ea549cbd37a428d2a12a63955adc9f4a
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-23 22:18:54 -0300

    aula 04 - video 03

--- argentum/src/main/java/br/com/alura/argentum/modelo/Candlestick.java
+++ argentum/src/main/java/br/com/alura/argentum/modelo/Candlestick.java
@@ -13,6 +13,11 @@ public final class Candlestick {
 
 	public Candlestick(double abertura, double fechamento, double maximo, double minimo, double volume,
 			LocalDateTime data) {
+		
+		if(maximo<minimo) {
+			throw new IllegalArgumentException("O máximo não deve ser menor que o mínimo");
+		}
+		
 		this.abertura = abertura;
 		this.fechamento = fechamento;
 		this.maximo = maximo;
@@ -46,7 +51,7 @@ public final class Candlestick {
 	}
 
 	public boolean isAlta() {
-		return this.fechamento > this.abertura;
+		return this.fechamento >= this.abertura;
 	}
 
 	public boolean isBaixa() {
--- argentum/src/test/java/br/com/alura/argentum/modelo/CandlestickTest.java
+++ argentum/src/test/java/br/com/alura/argentum/modelo/CandlestickTest.java
@@ -2,17 +2,41 @@ package br.com.alura.argentum.modelo;
 
 import java.time.LocalDateTime;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CandlestickTest {
 
-	@Test
-	public void test() {
+	@Test(expected = IllegalArgumentException.class)
+	public void maximoNaoDeveSerMenorDoQueOMinimo() {
+
 		CandleBuilder builder = new CandleBuilder();
+		LocalDateTime hoje = LocalDateTime.now();
 
-		Candlestick candle = builder.comAbertura(10.0).comFechamento(20.0).comMaximo(100.0).comMinimo(10.0)
-				.comVolume(100.0).comData(LocalDateTime.now()).geraCandle();
-
+		Candlestick candle = 
+				builder.comAbertura(10.0)
+				.comFechamento(30.0)
+				.comMinimo(25.0)
+				.comMaximo(15.0)
+				.comVolume(200)
+				.comData(hoje)
+				.geraCandle();
 	}
+	
+	@Test
+	public void ehAltaSeFechamentoForIgualAbertura() {
+		CandleBuilder builder = new CandleBuilder();
+		LocalDateTime hoje = LocalDateTime.now();
 
+		Candlestick candle = 
+				builder.comAbertura(30.0)
+				.comFechamento(30.0)
+				.comMinimo(25.0)
+				.comMaximo(50.0)
+				.comVolume(200)
+				.comData(hoje)
+				.geraCandle();
+		
+		Assert.assertTrue(candle.isAlta());
+	}
 }
