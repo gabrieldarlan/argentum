89692644d7836a42e627e0f1aaa3a433539b116d
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-25 12:20:45 -0300

    aula 03 video 02

--- argentum/src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
+++ argentum/src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
@@ -6,19 +6,36 @@ import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import org.primefaces.model.chart.LineChartModel;
+
+import br.com.alura.argentum.graficos.GeradorDeModeloGrafico;
+import br.com.alura.argentum.modelo.Candle;
+import br.com.alura.argentum.modelo.CandleFactory;
 import br.com.alura.argentum.modelo.Negociacao;
+import br.com.alura.argentum.modelo.SerieTemporal;
 import br.com.alura.argentum.ws.ClientWebservice;
 
 @ManagedBean
 @ViewScoped
-public class ArgentumBean  implements Serializable{
+public class ArgentumBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private List<Negociacao> negociacoes;
 
+	private LineChartModel modeloGrafico;
+
 	public ArgentumBean() {
 		this.negociacoes = new ClientWebservice().getNegociacoes();
+		List<Candle> candles = new CandleFactory().constroiCandles(negociacoes);
+		SerieTemporal serie = new SerieTemporal(candles);
+		GeradorDeModeloGrafico geradorDeModelo = new GeradorDeModeloGrafico(serie, 2, serie.getUltimaPosica());
+		geradorDeModelo.plotaMediaMovelSimples();
+		this.modeloGrafico = geradorDeModelo.getModeloGrafico();
+	}
+
+	public LineChartModel getModeloGrafico() {
+		return modeloGrafico;
 	}
 
 	public List<Negociacao> getNegociacoes() {
