080d05b45c5a65e6363b941ca9f71f840d37d674
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-24 21:50:53 -0300

    aula 01 - video 04

--- argentum/src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
+++ argentum/src/main/java/br/com/alura/argentum/bean/ArgentumBean.java
@@ -1,16 +1,27 @@
 package br.com.alura.argentum.bean;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.alura.argentum.modelo.Negociacao;
 import br.com.alura.argentum.ws.ClientWebservice;
 
 @ManagedBean
-public class ArgentumBean {
+@ViewScoped
+public class ArgentumBean  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private List<Negociacao> negociacoes;
+
+	public ArgentumBean() {
+		this.negociacoes = new ClientWebservice().getNegociacoes();
+	}
 
 	public List<Negociacao> getNegociacoes() {
-		return new ClientWebservice().getNegociacoes();
+		return this.negociacoes;
 	}
 }
